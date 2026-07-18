295625236b3b03e9f40b00bff7f42868d597ac74
Fry1997 <128788997+Fry1997@users.noreply.github.com>
2026-07-19 00:12:38 +0100

    Mark the normalised mobile layout experience

---
+++ together/shared/experience5.js
@@ -0,0 +1,6 @@
+(() => {
+  "use strict";
+  document.body.classList.add("experience-five");
+  document.documentElement.dataset.nearerExperience = "5";
+  window.__NEARER_EXPERIENCE5_STARTED = true;
+})();
