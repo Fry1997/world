3b18e3c02370a25abfbfd54c029df23493388609
Fry1997 <128788997+Fry1997@users.noreply.github.com>
2026-07-20 00:20:42 +0100

    Mark bundled cloud styling before platform startup

--- src/platform-entry.js
+++ src/platform-entry.js
@@ -1,7 +1,14 @@
 import "../cloud.css";
 
 window.__NEARER_PLATFORM_MODULE_PENDING = true;
-window.__NEARER_CLOUD_STYLES_BUNDLED = true;
+
+if (!document.querySelector("link[data-nearer-cloud-style]")) {
+  const bundledStyleMarker = document.createElement("link");
+  bundledStyleMarker.rel = "stylesheet";
+  bundledStyleMarker.media = "not all";
+  bundledStyleMarker.dataset.nearerCloudStyle = "bundled";
+  document.head.append(bundledStyleMarker);
+}
 
 try {
   await import("../cloud.js");
