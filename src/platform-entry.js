aa80d75aed9a3cfea78020608dc15e0dc36a8850
Fry1997 <128788997+Fry1997@users.noreply.github.com>
2026-07-20 01:55:37 +0100

    Start the navigation cache from the shared shell

--- src/platform-entry.js
+++ src/platform-entry.js
@@ -1,18 +1,19 @@
-import "../cloud.css";
+import '../cloud.css';
+import './service-worker-register.js';
 
 window.__NEARER_PLATFORM_MODULE_PENDING = true;
 
-if (!document.querySelector("link[data-nearer-cloud-style]")) {
-  const bundledStyleMarker = document.createElement("link");
-  bundledStyleMarker.rel = "stylesheet";
-  bundledStyleMarker.media = "not all";
-  bundledStyleMarker.dataset.nearerCloudStyle = "bundled";
+if (!document.querySelector('link[data-nearer-cloud-style]')) {
+  const bundledStyleMarker = document.createElement('link');
+  bundledStyleMarker.rel = 'stylesheet';
+  bundledStyleMarker.media = 'not all';
+  bundledStyleMarker.dataset.nearerCloudStyle = 'bundled';
   document.head.append(bundledStyleMarker);
 }
 
 try {
-  await import("../cloud.js");
-  await import("../platform.js");
+  await import('../cloud.js');
+  await import('../platform.js');
 } finally {
   window.__NEARER_PLATFORM_MODULE_PENDING = false;
 }
