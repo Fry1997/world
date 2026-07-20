cd2b442184f505a58636f28721c0a7faeb261db1
Fry1997 <128788997+Fry1997@users.noreply.github.com>
2026-07-20 22:35:32 +0100

    Load precision geometry only for Atlas

--- src/atlas-entry.js
+++ src/atlas-entry.js
@@ -1,2 +1,7 @@
 import './atlas-styles.js';
-import './atlas.js';
+import { initialiseGameRuntime } from './game-runtime.js';
+import { preparePrecisionGeometry } from './precision-geometry.js';
+
+await initialiseGameRuntime();
+await preparePrecisionGeometry();
+await import('./atlas.js');
