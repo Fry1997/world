4f79fd4371969ce8cbed7eab0709be524f0ed028
Fry1997 <128788997+Fry1997@users.noreply.github.com>
2026-07-25 22:45:55 +0100

    Avoid duplicating final surface CSS in Duel route assets

--- src/duel-styles.js
+++ src/duel-styles.js
@@ -17,5 +17,3 @@ import "../together/shared/experience9.css";
 import "../together/shared/experience10.css";
 import "../platform.css";
 import "../performance.css";
-import "../together/shared/experience11.css";
-import "../together/shared/experience11-clarity.css";
